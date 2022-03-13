7793eb19170bd4ae0f89302468151fe7298c6913
AJIT SHARMA <ajit08082000@gmail.com>
2022-03-13 13:31:02 +0530

    added some texts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-     <h2>this is how we are going to create a notes making app in 
-       a simple and easy way
-     </h2>
+      <h2>
+        {" "}
+        this is how we are going to create a notes making app in a simple and
+        easy way !{" "}
+      </h2>{" "}
     </div>
   );
 }
